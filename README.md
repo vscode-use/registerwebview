bfaa1bde2bb6b608935c0e21053ee009c27de36e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-04 20:55:46 +0800

    feat: add RegisterWebview

--- README.md
+++ README.md
@@ -2,6 +2,61 @@
 
 > WIP: 这个库是为了快速在 vscode 插件中使用 webview 打开侧边栏，让使用上更加简单
 
+## Usage
+
+```code
+import { RegisterWebview } from '@vscode-use/registerwebview'
+ // function activate
+ const provider = new RegisterWebview(
+  context.extensionUri,
+    `<div>hello,world</div>`, // html
+    ['main.css'], // css，本地css需要配置在media目录下
+    [], // js，本地js需要配置在media目录下
+    (data) => { // callback
+
+    }
+  )
+  vscode.window.registerWebviewViewProvider('calicoColors.colorsView', provider)
+```
+
+## Api
+
+- postMessage 推送消息给 js 层
+- deferScript 在 js 加载的最后去加载这部分的 script
+
+## Menu Settings
+
+```code
+  // package.json
+  "contributes": {
+    "views": {
+      "explorer": [
+        {
+          "type": "webview",
+          "id": "calicoColors.colorsView",
+          "name": "Calico Colors"
+        }
+      ]
+    },
+    "commands": [
+			{
+				"command": "calicoColors.clearColors",
+				"category": "Calico Colors",
+				"title": "Clear Colors"
+			}
+		],
+		"menus": {
+			"view/title": [ // 顶部右上角的按钮
+				{
+					"command": "calicoColors.clearColors",
+					"group": "navigation",
+					"when": "view == calicoColors.colorsView"
+				}
+			]
+		}
+  },
+```
+
 ## :coffee:
 
 [buy me a cup of coffee](https://github.com/Simon-He95/sponsor)
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,89 @@
-export const one = 1
-export const two = 2
+import * as vscode from 'vscode'
+
+export class RegisterWebview implements vscode.WebviewViewProvider {
+  private _view?: vscode.WebviewView
+  private _deferScript = ''
+  constructor(
+    private readonly _extensionUri: vscode.Uri,
+    private readonly _html: string,
+    private readonly _scripts: string | string[],
+    private readonly _styles: string | string[],
+    private readonly _callback: (data: any) => void,
+  ) {
+    this._extensionUri = _extensionUri
+    this._html = _html
+    this._scripts = _scripts
+    this._styles = _styles
+    this._callback = _callback
+  }
+
+  public resolveWebviewView(webviewView: vscode.WebviewView) {
+    this._view = webviewView
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    }
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview)
+
+    webviewView.webview.onDidReceiveMessage(this._callback)
+  }
+
+  public postMessage(data: any) {
+    if (this._view)
+      this._view.webview.postMessage(data)
+  }
+
+  deferScript(scripts: string | string[]) {
+    this._deferScript
+      = typeof scripts === 'string' ? scripts : scripts.join('\n')
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const outerUriReg = /^http[s]:\/\//
+    const styles = this._styles
+      ? (Array.isArray(this._styles) ? this._styles : [this._styles])
+          .map((style) => {
+            const styleUri = outerUriReg.test(style)
+              ? style
+              : webview.asWebviewUri(
+                vscode.Uri.joinPath(this._extensionUri, 'media', style),
+              )
+            return `<link href="${styleUri}" rel="stylesheet">`
+          })
+          .join('\n')
+      : ''
+    const scripts = (
+      Array.isArray(this._scripts) ? this._scripts : [this._scripts]
+    )
+      .map((script) => {
+        const scriptUri = outerUriReg.test(script)
+          ? script
+          : webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, 'media', script),
+          )
+        return script.startsWith('<script')
+          ? script
+          : `<script src="${scriptUri}"></script>`
+      })
+      .join('\n')
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          ${styles}
+          <title>registerwebview</title>
+        </head>
+        <body>
+          ${this._html}
+        </body>
+        ${scripts}
+        ${this._deferScript}
+			</html>`
+  }
+}
